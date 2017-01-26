f9bcb927e2577e98aea7be400d8ce9915d0d5b46
Elemino <elemshalit@gmail.com>
2017-01-25 21:35:55 -0800

    Update Models

--- models/Order.js
+++ models/Order.js
@@ -1,22 +1,13 @@
 var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
-/**
- * Order Model
- * ==========
- */
 var Order = new keystone.List('Order');
 
 Order.add({
-	customer:{ type:Types.Relationship, ref:'User', many:false, index:true, initial:true}
-  products:{ type:Types.Relationship, ref:'Product', many:true, index:true, initial:true,   },
-
+	customer: { type: Types.Relationship, ref: 'User', many: false, index: true,initial:true },
+	products: { type: Types.Relationship, ref: 'Product', many: true, index: true , required: true,initial:true},
 });
 
-
-/**
- * Registration
- */
- Order.defaultSort = '-createdAt';
- Order.defaultColumns = 'customer,Products';
- Order.register();
+Order.defaultSort = '-createdAt';
+Order.defaultColumns = 'customer, products';
+Order.register();
--- models/Product.js
+++ models/Product.js
@@ -1,27 +1,15 @@
 var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
-/**
- * Product Model
- * ==========
- */
 var Product = new keystone.List('Product');
 
 Product.add({
-	name: { type: Types.Text, required: true, initial: true },
-	price: { type: Types.Number, initial: true, required: true, },
-	description: { type: String }
+	name: { type: Types.Text, required: true,initial:true },
+	price: { type: Types.Number, required: true,initial:true },
+	description: { type: String },
+
 });
 
-// Provide access to Keystone
-Product.schema.virtual('canAccessKeystone').get(function () {
-	return this.isAdmin;
-});
-
-
-/**
- * Registration
- */
-
+Product.defaultSort = '-createdAt';
 Product.defaultColumns = 'name, price, description';
 Product.register();
