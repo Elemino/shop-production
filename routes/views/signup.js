ebc934838062bf387fca7831960599ec10a42585
Elemino <elemshalit@gmail.com>
2017-12-15 23:20:44 -0800

    Add Purchase.js

---
+++ routes/views/signup.js
@@ -0,0 +1,20 @@
+var keystone = require('keystone');
+var User = keystone.list('users');
+var Order - keystone.list('orders');
+exports = module.exports = function(req, res) {
+
+  var view = new keystone.View(req, res);
+  var locals = res.locals;
+
+  console.log("...")+JSON.stringify(req.body));
+
+      console.log(req.body.email);
+      var newUser = User,model({
+        name: {first: req.body.first,last: req.body,last},
+        email: req.body.email,
+        password: req.body.password,
+        isAdmin: fasle
+      });
+
+
+}
