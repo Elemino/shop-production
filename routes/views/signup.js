8b33e5a7fe7df0d26735fcd4f2148de60e4a0aab
Elemino <elemshalit@gmail.com>
2017-12-22 17:19:45 -0800

    Add callhook and signin session

--- routes/views/signup.js
+++ routes/views/signup.js
@@ -6,10 +6,15 @@ exports = module.exports = function(req, res) {
   var view = new keystone.View(req, res);
   var locals = res.locals;
 
+
+  //locals
   console.log("...")+JSON.stringify(req.body));
 
+
+  console.log(req.body.first+" "+req.body.last);
+
       console.log(req.body.email);
-      var newUser = User,model({
+      var newUser = User.model({
         name: {first: req.body.first,last: req.body,last},
         email: req.body.email,
         password: req.body.password,
@@ -19,10 +24,39 @@ exports = module.exports = function(req, res) {
   var updater = newUser.getUpdateHandler (req);
   var my cart = req.session.cat;
   updater.process(req, body, {
-    fields: 'email'
-    flashErrors: true,
-    logErrors: true
-  })
+      fields: 'email'
+      flashErrors: true,
+      logErrors: true
+  }, function(err,result) {
 
+      if (err) {
 
-}
+          console.log(err);
+      } else {
+        keystone.session.signinWithUser (newUser, req, res, function (){
+          console.log("signinWithUser func");
+          keystone.callHook(newUser, 'post:signin' function (err){
+              if (err) {
+                console.log(err);
+              } else {
+                console.log('signedin');
+                if(mycart !=undefined ){
+                        mycart_pro_ids = [];
+                        mycart.forEach( function (product)
+                        {
+                          mycart_pro_ids.push(product._id);
+                        });
+                        console.log("...".JSON.stringify(mycart_pro_ids));
+                    var newOrder = Order.model({
+                        customer: req.user.id,
+                        products: mycart_pro_ids,
+
+                    });
+                   }
+                  }
+                 }
+                }
+               }
+              }
+             }
+            }
