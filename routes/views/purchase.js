655a8f7b98cf0998318241738ae5f57ea54d4f38
Elemino <elemshalit@gmail.com>
2017-12-24 22:43:16 -0800

    Add purchase.js

---
+++ routes/views/purchase.js
@@ -0,0 +1,37 @@
+var keystone = require ('keystone');
+
+var Order = keystone.list('orders');
+exports = module.exports = function(req, res) {
+
+    var view = new keystone.View(req, res);
+    var locals = res.locals;
+
+    if(req.session.cart !=undefined){
+        mycart_pro_ids = [];
+        req.session.cart.forEach( function (product){
+
+            mycart_pro_ids.push(product._id);
+        });
+
+        var newOrder = Order.model({customer: req.user.id,product: mycart_pro_ids});
+        updater = newOrder.getUpdateHandler(req, res, {
+            errorMessage: 'Your order encoutered an error'
+          });
+
+          updater.process(req, body, {
+              flashErrors: true,
+              logErrors: true,
+          }, function(err) {
+              if (err) {
+
+              } else {
+
+                  req.session.cart=[];
+                  res.redirect('/myorders')
+              }
+
+          });
+    }else{
+        res.redirect('/')
+    }
+};
