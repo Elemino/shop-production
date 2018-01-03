dc0700b41eedb42a04d86e715535a474a2a70370
Elemino <elemshalit@gmail.com>
2018-01-02 20:13:07 -0800

    Add mycart view and fix some indentation plus some edits in signup.js

--- routes/views/add_to_cart.js
+++ routes/views/add_to_cart.js
@@ -1,4 +1,3 @@
-
 var keystone = require('keystone');
 
 exports = module.exports = function (req, res) {
@@ -7,6 +6,7 @@ exports = module.exports = function (req, res) {
     var q = keystone.list('Product').model.findById(req.query.product_id);
 
     q.exec(function(err, result) {
+
       var product = result;
       req.session.cart.push(result);
       res.send('succeess')
