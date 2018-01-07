c5fde18eef5167aadc3c16d2838c772879518f0e
Elemino <elemshalit@gmail.com>
2018-01-06 16:52:35 -0800

    Remove comments in stripe.js

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,11 +1,11 @@
-// Handle form submission
+
 var form = document.getElementById('payment-form');
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
 stripe.createToken(card).then(function(result) {
     if (result.error) {
-        // Inform the user if there was an error
+
         var errorElement = document.getElementById('card-errors');
         errorElement.textContent = result.error.message;
     } else {
@@ -15,7 +15,7 @@ stripe.createToken(card).then(function(result) {
     });
 });
 var onReceiveToken = function(token, args) {
-    // Submit token to server so it can charge the card
+
     $.ajax({
         url: '/checkout',
         type: 'POST',
