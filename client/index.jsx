1a2bfcd6da41912723c57f9448b2d775cfa7081b
Elemino <elemshalit@gmail.com>
2018-08-19 20:20:56 -0700

    Update package.json and add index.jsx

---
+++ client/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+    <h1> Parts list</h1>
+  
+      <img style={{width: '300px', height: '300px'}}src="parts.jpg"/>
+    <h2>Item List</h2>
+    <ul>
+      <li>First component</li>
+      <li>Second component</li>
+      <li>Third Component</li>
+    </ul>
+    <h2> Checkout Instructions </h2>
+    <p> Add checkout instructions here </p>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('.react-container'));
