f93536dcb54e5d276347e0c91a095971904e2b3a
Elemino <elemshalit@gmail.com>
2018-08-23 14:50:20 -0700

    Change entry and style to convention

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,33 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
- 
- mode: 'development',
-	
+
+	mode: 'development',
+
 	entry: [
-		'./client/index.jsx',
+		'./index.jsx',
 	],
-	
+
 	output: {
 		path: path.join(__dirname, '/server/public/js/'),
 		filename: 'bundle.js',
 		publicPath: 'server/public/js/',
-  },
-  
+	},
+
 	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [
-							'react',
-							'env',
-						],
-					},
+		rules: [{
+			test: /\.(js|jsx)$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: [
+						'react',
+						'env',
+					],
 				},
-      },
-    ]
+			},
+		}, ]
 	},
 	resolve: {
 		extensions: ['.js', '.jsx', '.scss'],
