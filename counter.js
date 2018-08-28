c060275403dd051c4744a6be6e14d70f1926cc0f
Elemino <elemshalit@gmail.com>
2018-08-27 23:38:51 -0700

    Add format count and remove eslint

--- counter.js
+++ counter.js
@@ -1,6 +1,4 @@
-import React, {
-	Component
-} from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
 	state = {
@@ -9,16 +7,14 @@ class Counter extends Component {
 
 	render() {
 		return (
-
-			<
-			div >
-			<
-			span > {
-				this.state.count
-			} < /span> <
-			button > Increment < /button> <
-			/div>
+			<div>
+				<span> {this.state.count}</span>
+				<button>Increment</button>
+			</div>
 		);
+	}
+	formatCount() {
+		return this.state.count === 0 ? "Zero" : this.state.count;
 	}
 }
 
