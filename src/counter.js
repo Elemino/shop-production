e45f573ab147807b08e26d6abfd783c408e2e42e
Elemino <elemshalit@gmail.com>
2018-09-17 23:51:29 -0700

    Add increment handler

--- src/counter.js
+++ src/counter.js
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 
 class Counter extends Component {
 	state = {
-		count: 1
+		count: 0
 	};
+
+	handleIncrement() {
+		console.log("Increment!", this)
+	}
 
 	render() {
 		return (
 			<div>
 				<span className="Badge badge-primary m-2">{this.format.Count()}</span>
-				<button className="btn btn-secondary btn-sm">Increment</button>
+				<button
+				onClick={this.handleIncrement}
+				className="btn btn-secondary btn-sm">Increment</button>
 			</div>
 		);
 	}
