b609258639b10a77df4b546987ffe900a95544e9
Elemino <elemshalit@gmail.com>
2019-04-16 23:54:01 -0700

    Update packages and switch from vars to lets

--- routes/views/fund.js
+++ routes/views/fund.js
@@ -1,18 +1,18 @@
-var keystone = require('keystone');
-var async = require('async');
+const keystone = require('keystone');
+const async = require('async');
 
-var Order = keystone.list();
-var OrderItem = keystone.list('OrderItem');
-exports = module.exports = function(req, res) {
+let Order = keystone.list();
+let OrderItem = keystone.list('OrderItem');
+exports = module.exports = function (req, res) {
 
-    var view = new keystone.View(req,res);
-    var locals = res.locals;
+	let view = new keystone.View(req, res);
+	let locals = res.locals;
 
-    var final = function (){
-        res.redirect('/');
-    }
+    let final = function () {
+	res.redirect('/');
+};
 
-    if(req.session.cart !=undefined ){
+	if (req.session.cart != undefined ){
         mycart_item_ids = [];
         async.forEach(req.session.cart, function (item, callback){
             console.log(JSON.stringify(item));
